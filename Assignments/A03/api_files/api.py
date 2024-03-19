31c47f498991794f60716dbc06a36a6d2163f1ab
Ahmed <ahmedkrubally696@gmail.com>
2024-03-19 12:58:03 -0500

    commit

--- Assignments/A03/api_files/api.py
+++ Assignments/A03/api_files/api.py
@@ -249,4 +249,4 @@ Note:
     The right side (app) is the bearingiable name of the FastApi instance declared at the top of the file.
 """
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="127.0.0.1", port=8080, log_level="debug", reload=True)
+    uvicorn.run("api:app", host="137.184.81.103", port=8080, log_level="debug", reload=True)
